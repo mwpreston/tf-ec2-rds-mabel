d6c5143f708166043dcb1dc742dc76095feb5caa
Mike Preston <mwpreston@gmail.com>
2020-10-20 09:42:22 -0400

    updates

--- files/mabel.sql
+++ files/mabel.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
---
--- Host: estoredb.czledfewf5tq.us-east-1.rds.amazonaws.com    Database: eStoreDB
--- ------------------------------------------------------
--- Server version	5.7.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
--- files/wp-setup.sh
+++ files/wp-setup.sh
@@ -8,21 +8,7 @@ sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-gd php5
 sudo apt-get install php5-mysqlnd-ms -y
 sudo apt-get install php-gd php-mysql libapache2-mod-php -y
 sudo service apache2 restart
-sudo wget -c http://wordpress.org/wordpress-5.1.1.tar.gz
-sudo tar -xzvf wordpress-5.1.1.tar.gz
 sleep 20
-sudo mkdir -p /var/www/html/
-sudo rsync -av wordpress/* /var/www/html/
-sudo chown -R www-data:www-data /var/www/html/
-sudo chmod -R 755 /var/www/html/
-sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-sudo service apache2 restart
-echo "${db_hostname}" >> /home/ubuntu/sedcmd
-sudo sed -i 's/localhost/${db_hostname}/g' /var/www/html/wp-config.php >> /home/ubuntu/sedcmd
-sudo sed -i 's/database_name_here/${db_name}/g' /var/www/html/wp-config.php >> /home/ubuntu/sedcmd
-sudo sed -i 's/username_here/${db_user}/g' /var/www/html/wp-config.php >> /home/ubuntu/sedcmd
-sudo sed -i 's/password_here/${db_password}/g' /var/www/html/wp-config.php >> /home/ubuntu/sedcmd
-sudo rm -fr /var/www/html/index.html
 cat <<EOT >> ~/.my.cnf
 [mysqldump]
 host = ${db_hostname}
@@ -39,6 +25,13 @@ EOT
 chmod 600 ~/.my.cnf
 mkdir /home/ubuntu/git
 git clone git clone https://github.com/mwpreston/tf-ec2-rds-mabel.git /home/ubuntu/git
-mysql {$db_name}
-
-
+mysql {$db_name} < /home/ubuntu/git/files/mabel.sql
+sudo mkdir -p /var/www/html/
+sudo tar -xzvf /home/ubuntu/git/files/wordpress.tar.gz -C /var/www/html/
+sudo sed -i 's/localhost/${db_hostname}/g' /var/www/html/wp-config.php
+sudo sed -i 's/database_name_here/${db_name}/g' /var/www/html/wp-config.php
+sudo sed -i 's/username_here/${db_user}/g' /var/www/html/wp-config.php
+sudo sed -i 's/password_here/${db_password}/g' /var/www/html/wp-config.php
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
+sudo service apache2 restart
